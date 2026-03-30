e9cb643b2706d171d9c57003582a041aaf7b05b3
gonerick2000-max <gonerick2000@gmail.com>
2026-03-30 17:21:00 +0200

    Update init_tables.sql

--- scripts/init_tables.sql
+++ scripts/init_tables.sql
@@ -4,20 +4,15 @@ CREATE TABLES
 =============
 Script Purpose:
 This script creates a table for each csv data file extrated from our sources: source_crm and source_erp.
+The 4th column of bronze.crm_sales_details table accepts nvarchar(8) values although data is supposed to contain
+date values. In this way we avoid loading issues and we will manage them later.
 */
 
 USE DataWareHouse
 
 
-DROP TABLE IF EXISTS bronze.crm_cust_info;
-DROP TABLE IF EXISTS bronze.crm_prd_info;
-DROP TABLE IF EXISTS bronze.crm_sales_details;
-DROP TABLE IF EXISTS bronze.erp_CUST_AZ12;
-DROP TABLE IF EXISTS bronze.erp_LOC_A101;
-DROP TABLE IF EXISTS bronze.PX_CAT_G1V2;
-
 /* ----------------------------- */
-
+DROP TABLE IF EXISTS bronze.crm_cust_info;
 CREATE TABLE bronze.crm_cust_info (
 	cst_id INT,
 	cst_key NVARCHAR(10),
@@ -29,7 +24,7 @@ CREATE TABLE bronze.crm_cust_info (
 );
 
 /* ----------------------------- */
-
+DROP TABLE IF EXISTS bronze.crm_prd_info;
 CREATE TABLE bronze.crm_prd_info (
 	prd_id INT,
 	prd_key NVARCHAR(50),
@@ -41,12 +36,12 @@ CREATE TABLE bronze.crm_prd_info (
 );
 
 /* ----------------------------- */
-
+DROP TABLE IF EXISTS bronze.crm_sales_details;
 CREATE TABLE bronze.crm_sales_details (
 	sls_ord_num NVARCHAR(50),
 	sls_prd_key NVARCHAR(50),
 	sls_cust_id INT,
-	sls_order_dt DATE,
+	sls_order_dt NVARCHAR(8),
 	sls_ship_dt DATE,
 	sls_due_dt DATE,
 	sls_sales INT,
@@ -55,7 +50,7 @@ CREATE TABLE bronze.crm_sales_details (
 );
 
 /* ----------------------------- */
-
+DROP TABLE IF EXISTS bronze.erp_CUST_AZ12;
 CREATE TABLE bronze.erp_CUST_AZ12 (
 	CID NVARCHAR(50),
 	BDATE DATE,
@@ -63,18 +58,17 @@ CREATE TABLE bronze.erp_CUST_AZ12 (
 );
 
 /* ----------------------------- */
-
+DROP TABLE IF EXISTS bronze.erp_LOC_A101;
 CREATE TABLE bronze.erp_LOC_A101 (
 	CID NVARCHAR(50),
 	CNTRY NVARCHAR(50)
 );
 
 /* ----------------------------- */
-
-CREATE TABLE bronze.PX_CAT_G1V2 (
+DROP TABLE IF EXISTS bronze.erp_PX_CAT_G1V2;
+CREATE TABLE bronze.erp_PX_CAT_G1V2 (
 	ID NVARCHAR(50),
 	CAT NVARCHAR(50),
 	SUBCAT NVARCHAR(50),
 	MAINTENANCE NVARCHAR(10)
 );
-
